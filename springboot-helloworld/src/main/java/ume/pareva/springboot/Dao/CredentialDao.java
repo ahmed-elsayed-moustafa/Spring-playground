838a33cdedd5dfbd899e621dae3609ace76275f8
Ahmed Elsayed <Ahmed@Ahmeds-MBP.lan>
2017-10-04 20:02:29 +0100

    removed projects

--- springboot-helloworld/src/main/java/ume/pareva/springboot/Dao/CredentialDao.java
+++
@@ -1,9 +0,0 @@
-package ume.pareva.springboot.Dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ume.pareva.springboot.models.CredentialService;
-
-public interface CredentialDao extends CrudRepository<CredentialService, Integer>{
-
-}
--- springboot-helloworld/src/main/java/ume/pareva/springboot/Dao/SubscriptionDao.java
+++
@@ -1,9 +0,0 @@
-package ume.pareva.springboot.Dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import ume.pareva.springboot.models.SubscriptionNotification;
-
-public interface SubscriptionDao extends CrudRepository<SubscriptionNotification, Integer> {
-
-}
